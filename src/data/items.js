5298bba0f25556fe0d127f268567bddb797b2a83
Garret Gallant <gallanttechnological@gmail.com>
2022-07-21 11:36:24 -0600

    commit tree 81ff20c13fc245d93cbd91c812464ee0313ab1dd

--- src/data/items.js
+++ src/data/items.js
@@ -4,6 +4,10 @@ const items = [
   { id: 3, name: "Lettuce", category: "Produce" },
   { id: 4, name: "String Cheese", category: "Dairy" },
   { id: 5, name: "Cookies", category: "Dessert" },
+  { id: 6, name: "Not Cookies", category: "Not Dessert" },
+  { id: 7, name: "Not String Cheese", category: "Not Dairy" },
+  { id: 8, name: "Lettuce", category: "Not Produce" },
+  { id: 9, name: "Yogurt", category: "Not Dairy" }
 ];
 
 export default items;
